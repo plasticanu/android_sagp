ea7de3c96091c259f4c74bb01d8190e9caa2e770
Jun Cheng Zhang <u7297753@anu.edu.au>
2022-04-29 17:49:17 +1000

    add loadData method

--- app/src/main/java/com/example/comp6442_group_assignment/MainActivity.java
+++ app/src/main/java/com/example/comp6442_group_assignment/MainActivity.java
@@ -113,4 +113,25 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setTitle(title);
     }
 
+    /**
+     * A method used to read data from a file in the assets folder
+     * @Author Jun Cheng Zhang u7297753
+     * @param fileName the file's name
+     * @return the data in the file
+     */
+    public String loadData(String fileName){
+        String data = "";
+        try {
+            InputStream inputStream = getAssets().open(fileName);
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+            inputStream.close();
+            data = new String(buffer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
 }
