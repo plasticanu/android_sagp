94bf2c4e311b053687ea5167d679579e5f46552d
Jared Piuh <u7139999@anu.edu.au>
2022-05-15 07:05:42 +1000

    library reference

--- app/src/main/java/com/example/comp6442_group_assignment/Search/Search.java
+++ app/src/main/java/com/example/comp6442_group_assignment/Search/Search.java
@@ -21,7 +21,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
