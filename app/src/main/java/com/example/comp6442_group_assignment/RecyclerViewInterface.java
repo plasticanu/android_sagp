e17376467efc2443b2cd4e1b7334fb637af53294
antungchen <chenantung@gmail.com>
2022-05-18 00:43:07 +1000

    UI framework V3.1: Users can now like the posts if they are logged in

--- app/src/main/java/com/example/comp6442_group_assignment/RecyclerViewInterface.java
+++ app/src/main/java/com/example/comp6442_group_assignment/RecyclerViewInterface.java
@@ -1,6 +1,8 @@
 package com.example.comp6442_group_assignment;
 
 public interface RecyclerViewInterface {
+    //used to perform item click event
     void onItemClick(int position);
+    //used to perform like post event
     void onLikeClick(int position);
 }
